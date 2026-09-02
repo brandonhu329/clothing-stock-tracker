ec4f3a332bd2b2dcab2f0c2332c8032de55cbb17
Brandon Hu <brandonhu32@g.ucla.edu>
2026-09-02 16:38:09 -0700

    Updated tracker file

--- tracker.py
+++ tracker.py
@@ -1,9 +1,7 @@
 from playwright.sync_api import sync_playwright
+
 from hollister import check_stock
 from notifications import send_notification
-from config import CHECK_EVERY
-
-import time
 
 
 with sync_playwright() as p:
@@ -11,31 +9,15 @@ with sync_playwright() as p:
 
     context = browser.new_context()
 
-    while True:
-        try:
-            in_stock = check_stock(context)
+    try:
+        in_stock = check_stock(context)
 
-            if in_stock:
-                send_notification()
-                break
+        if in_stock:
+            send_notification()
 
-            print()
-            print("Checking again in 5 minutes...")
+    except Exception as error:
+        print("Something went wrong:")
+        print(error)
 
-            time.sleep(CHECK_EVERY)
-
-        except KeyboardInterrupt:
-            print()
-            print("Tracker stopped")
-            break
-
-        except Exception as error:
-            print()
-            print("Something went wrong:")
-            print(error)
-
-            print("Trying again in 5 minutes...")
-
-            time.sleep(CHECK_EVERY)
-
-    browser.close()
+    finally:
+        browser.close()
